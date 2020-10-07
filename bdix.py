ae1196ebdafac44f532a62c3c8ae6bcf3403dd40
Shantanu Dey Anik <40882871+Shantanu2645@users.noreply.github.com>
2020-10-08 02:48:56 +0600

    This is the main script for BDIX test

--- bdix.py
+++ bdix.py
@@ -18,19 +18,23 @@ timer = int(input("Enter timeout based on your internet speed (second): "))
 
 bdix_list = url_list.read().split()
 
-url_list.close
+url_list.close()
 
+count = len(bdix_list)
 print("\nSearching from",len(bdix_list),"sites\n")
 print("\nThis BDIX servers will work for your ISP:-\n")
 bdix_list_final = []
 
 for i in bdix_list:
+    count = count - 1
     try:
         request_response = requests.head(i,timeout=timer)
         
         if request_response.status_code == 200 :
             bdix_list_final.append(i)
-            print(i)
+            out = i + "   ("+count+" remaining)"
+            print(out)
+            
     except requests.exceptions.Timeout: 
         pass
     except requests.exceptions.ConnectionError:
@@ -54,19 +58,4 @@ final_urls.close()
 
 print("\nCheck 'data.txt' file for working URLS\nEnjoy!! \nCreated and Maintained By Shantanu Dey Anik")
 print("\nFacebook: www.facebook.com/shantanu.anik.dey ")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
